0ddbe05b11ff5f488dc84796d4894805c7fe0933
Ciao Chung <ciao0958@gmail.com>
2018-08-12 00:47:19 +0800

    add Cache Module

--- Server/src/Modules/Cache/Cache.js
+++ Server/src/Modules/Cache/Cache.js
@@ -1,6 +1,36 @@
+import cacheManager from 'cache-manager'
+import fsStore from 'cache-manager-fs'
 class Cache {
   async init(config) {
     this.config = config
+    this.cacheManager = cacheManager.caching({
+      store: fsStore,
+      options: {
+        ttl: this._getCacheConfigProperty('ttl')*1000,
+        maxsize: this._getCacheConfigProperty('maxsize')*1000,
+        path:'diskcache',
+        preventfill:true,
+      }
+    })
+  }
+
+  _getCacheConfigProperty(property) {
+    if(!this.config) return null
+    if(!this.config.cache) return null
+    if(!this.config.cache[property]) return null
+    return this.config.cache[property]
+  }
+
+  get(key) {
+    this.cacheManager.get(key)
+  }
+
+  set(key, value) {
+    this.cacheManager.set(key, value)
+  }
+
+  del(key) {
+    this.cacheManager.del(key)
   }
 }
 
