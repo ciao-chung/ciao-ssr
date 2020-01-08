5f3a02cbd3acfd747ed42a0342e0898eace9ffd2
Ciao Chung <ciao0958@gmail.com>
2020-01-08 15:50:51 +0800

    chore: handle browser crash exception

--- Server/src/Modules/Crawler/Crawler.js
+++ Server/src/Modules/Crawler/Crawler.js
@@ -1,30 +1,31 @@
 const puppeteer = require('puppeteer')
-const child_process = require('child_process')
 const Page = require('./Page')
 class Crawler {
   async init(config) {
     this.debug = config.debug == true
     const customLaunchOptions = typeof config.launchOptions == 'object' ? config.launchOptions : {}
-    this.browser = await puppeteer.launch({
+    this.launchOptins = {
       headless: !this.debug,
       ...customLaunchOptions,
-    })
-
-    this.browser.on('disconnected', this._handleBrowserDisconnected)
+    }
+    this._launchBrowser()
   }
 
-  async _handleBrowserDisconnected() {
-    const processPid = process.pid
-    log(`Handle Browser Disconnected, Process: ${processPid}`, 'red')
-    setTimeout(() => {
-      child_process.exec(`kill -9 ${processPid}`, (error, stdout, stderr) => {
-        if (error) {
-          log(`Process Kill Error: ${error}`, 'red')
-        }
+  async _launchBrowser() {
+    this.browser = await puppeteer.launch(this.launchOptins)
+    this.browser.on('disconnected', () => this._restartBrowser())
+  }
 
-        log(`Process Kill Success. stdout: ${stdout} stderr:${stderr}`, 'red')
-      })
-    }, 500)
+  async _restartBrowser() {
+    log(`Restart browser`, 'red')
+    try {
+      this.browser.close()
+    } catch(error) {
+
+    }
+    setTimeout(async () => {
+      this._launchBrowser()
+    }, 1000)
   }
 
   async render(url) {
